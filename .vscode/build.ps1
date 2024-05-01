302f18b12431bbb44e62e96e910e06842776495f
Alexander Krivács Schrøder <alexschrod@gmail.com>
2024-05-01 09:35:53 +0200

    chore: switch to LoadFolders

--- .vscode/build.ps1
+++ .vscode/build.ps1
@@ -4,7 +4,8 @@ $Target = "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\Realistic
 
 # build dlls
 $env:RimWorldVersion = "1.5"
-dotnet build --configuration Release .vscode
+dotnet build .vscode
+#dotnet build --configuration Release .vscode
 if ($LASTEXITCODE -gt 0) {
     throw "Build failed"
 }
@@ -16,8 +17,6 @@ Remove-Item -Path $Target -Recurse -ErrorAction SilentlyContinue
 Copy-Item -Path 1.5 $Target\1.5 -Recurse
 
 Copy-Item -Path Common $Target\Common -Recurse
-Copy-Item -Path Defs $Target\Defs -Recurse
-Copy-Item -Path Patches $Target\Patches -Recurse
 
 New-Item -Path $Target -ItemType Directory -Name About
 Copy-Item -Path About\About.xml $Target\About
