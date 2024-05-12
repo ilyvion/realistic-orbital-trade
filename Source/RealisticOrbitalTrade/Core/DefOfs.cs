95cf1052b2a3050746936d006bfd6225257b47d6
Alexander Krivács Schrøder <alexschrod@gmail.com>
2024-05-12 07:13:52 +0200

    refactor: add DefOf safety checks

--- Source/RealisticOrbitalTrade/Core/DefOfs.cs
+++ Source/RealisticOrbitalTrade/Core/DefOfs.cs
@@ -10,6 +10,11 @@ internal static class QuestScriptDefOf
 {
     public static QuestScriptDef ROT_TradeShipTransportShip;
     public static QuestScriptDef ROT_TradeShipMakeAmends;
+
+    static QuestScriptDefOf()
+    {
+        DefOfHelper.EnsureInitializedInCtor(typeof(QuestScriptDefOf));
+    }
 }
 
 [DefOf]
@@ -18,10 +23,20 @@ internal static class ThingDefOf
     public static ThingDef ROT_TradeShuttle;
     public static ThingDef ROT_ExplosiveRiggedTradeShuttle;
     public static ThingDef ROT_ExplosiveRiggedAmendmentTradeShuttle;
+
+    static ThingDefOf()
+    {
+        DefOfHelper.EnsureInitializedInCtor(typeof(ThingDefOf));
+    }
 }
 [DefOf]
 internal static class ShipJobDefOf
 {
     public static ShipJobDef ROT_CancelLoad;
+
+    static ShipJobDefOf()
+    {
+        DefOfHelper.EnsureInitializedInCtor(typeof(ShipJobDefOf));
+    }
 }
 #pragma warning restore CS8618,CS0649
