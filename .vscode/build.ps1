fa9bba06484d55284b3ca59f97fe68ff15d110e6
Alexander Krivács Schrøder <alexschrod@gmail.com>
2024-06-03 07:55:37 +0200

    chore: upgrade to improved project experience

--- .vscode/build.ps1
+++ .vscode/build.ps1
@@ -1,36 +1,16 @@
 $ErrorActionPreference = 'Stop'
 
-$Configuration = 'Release'
+$env:RimWorldVersion = $args[0]
+$Configuration = 'Debug'
 
-$Target = "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\RealisticOrbitalTrade"
+$VersionTargetPrefix = "D:\RimWorld"
+$VersionTargetSuffix = "Mods\RealisticOrbitalTrade"
+$Target = "$VersionTargetPrefix\$env:RimWorldVersion\$VersionTargetSuffix"
 
-# $env:RimWorldSteamWorkshopFolderPath = "..\.deps\refs"
-$env:RimWorldSteamWorkshopFolderPath = "C:\Program Files (x86)\Steam\steamapps\workshop\content\294100"
+$env:RimWorldSteamWorkshopFolderPath = "..\.deps\refs"
+# $env:RimWorldSteamWorkshopFolderPath = "C:\Program Files (x86)\Steam\steamapps\workshop\content\294100"
 
 # build dlls
-$env:RimWorldVersion = "1.5"
-dotnet build --configuration $Configuration .vscode/mod.csproj
-if ($LASTEXITCODE -gt 0) {
-    throw "Build failed"
-}
-dotnet build --configuration $Configuration .vscode/wehadatrader.interop.csproj
-if ($LASTEXITCODE -gt 0) {
-    throw "Build failed"
-}
-dotnet build --configuration $Configuration .vscode/tweaksgalore.interop.csproj
-if ($LASTEXITCODE -gt 0) {
-    throw "Build failed"
-}
-dotnet build --configuration $Configuration .vscode/dynamictradeinterface.interop.csproj
-if ($LASTEXITCODE -gt 0) {
-    throw "Build failed"
-}
-dotnet build --configuration $Configuration .vscode/autoseller.interop.csproj
-if ($LASTEXITCODE -gt 0) {
-    throw "Build failed"
-}
-
-$env:RimWorldVersion = "1.4"
 dotnet build --configuration $Configuration .vscode/mod.csproj
 if ($LASTEXITCODE -gt 0) {
     throw "Build failed"
@@ -48,38 +28,27 @@ if ($LASTEXITCODE -gt 0) {
     throw "Build failed"
 }
 
-# remove pdbs (for release)
-if ($Configuration -eq "Release") {
-    Remove-Item -Path .\1.5\Assemblies\RealisticOrbitalTrade.pdb -ErrorAction SilentlyContinue
-    Remove-Item -Path .\1.4\Assemblies\RealisticOrbitalTrade.pdb -ErrorAction SilentlyContinue
-
-    Remove-Item -Path .\1.5_WeHadATrader\Assemblies\RealisticOrbitalTrade.WeHadATrader.pdb -ErrorAction SilentlyContinue
-    Remove-Item -Path .\1.4_WeHadATrader\Assemblies\RealisticOrbitalTrade.WeHadATrader.pdb -ErrorAction SilentlyContinue
-
-    Remove-Item -Path .\1.5_TweaksGalore\Assemblies\RealisticOrbitalTrade.TweaksGalore.pdb -ErrorAction SilentlyContinue
-
-    Remove-Item -Path .\1.5_DynamicTradeInterface\Assemblies\RealisticOrbitalTrade.DynamicTradeInterface.pdb -ErrorAction SilentlyContinue
-    Remove-Item -Path .\1.4_DynamicTradeInterface\Assemblies\RealisticOrbitalTrade.DynamicTradeInterface.pdb -ErrorAction SilentlyContinue
-
-    Remove-Item -Path .\1.5_AutoSeller\Assemblies\RealisticOrbitalTrade.AutoSeller.pdb -ErrorAction SilentlyContinue
-    Remove-Item -Path .\1.4_AutoSeller\Assemblies\RealisticOrbitalTrade.AutoSeller.pdb -ErrorAction SilentlyContinue
+if ($env:RimWorldVersion -eq "1.5") {
+    dotnet build --configuration $Configuration .vscode/tweaksgalore.interop.csproj
+    if ($LASTEXITCODE -gt 0) {
+        throw "Build failed"
+    }
 }
 
 # remove mod folder
 Remove-Item -Path $Target -Recurse -ErrorAction SilentlyContinue
 
 # copy mod files
-Copy-Item -Path 1.5 $Target\1.5 -Recurse
-Copy-Item -Path 1.4 $Target\1.4 -Recurse
+Copy-Item -Path $env:RimWorldVersion $Target\$env:RimWorldVersion -Recurse
 
 # copy interop mod files
-Copy-Item -Path 1.5_WeHadATrader $Target\1.5_WeHadATrader -Recurse
-Copy-Item -Path 1.4_WeHadATrader $Target\1.4_WeHadATrader -Recurse
-Copy-Item -Path 1.5_TweaksGalore $Target\1.5_TweaksGalore -Recurse
-Copy-Item -Path 1.5_DynamicTradeInterface $Target\1.5_DynamicTradeInterface -Recurse
-Copy-Item -Path 1.4_DynamicTradeInterface $Target\1.4_DynamicTradeInterface -Recurse
-Copy-Item -Path 1.5_AutoSeller $Target\1.5_AutoSeller -Recurse
-Copy-Item -Path 1.4_AutoSeller $Target\1.4_AutoSeller -Recurse
+Copy-Item -Path ${env:RimWorldVersion}_WeHadATrader $Target\${env:RimWorldVersion}_WeHadATrader -Recurse
+Copy-Item -Path ${env:RimWorldVersion}_DynamicTradeInterface $Target\${env:RimWorldVersion}_DynamicTradeInterface -Recurse
+Copy-Item -Path ${env:RimWorldVersion}_AutoSeller $Target\${env:RimWorldVersion}_AutoSeller -Recurse
+
+if ($env:RimWorldVersion -eq "1.5") {
+    Copy-Item -Path ${env:RimWorldVersion}_TweaksGalore $Target\${env:RimWorldVersion}_TweaksGalore -Recurse
+}
 
 Copy-Item -Path Common $Target\Common -Recurse
 Copy-Item -Path UpdateLog $Target\UpdateLog -Recurse
@@ -97,3 +66,6 @@ Copy-Item -Path LICENSE.Apache-2.0 $Target
 Copy-Item -Path LICENSE.MIT $Target
 Copy-Item -Path README.md $Target
 Copy-Item -Path LoadFolders.xml $Target
+
+# Trigger auto-hotswap
+New-Item -Path $Target\$env:RimWorldVersion\Assemblies\RealisticOrbitalTrade.dll.hotswap -Type file
---
+++ .vscode/buildAll.ps1
@@ -0,0 +1,2 @@
+Invoke-Expression "$PSScriptRoot\build.ps1 1.5"
+Invoke-Expression "$PSScriptRoot\build.ps1 1.4"
